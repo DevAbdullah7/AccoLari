d8a52223f1a21beea62002e8cc092dfa30b1aed5
Abdullah Alqahtani <67982210+DevAbdullah7@users.noreply.github.com>
2024-07-08 16:47:56 +0300

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 export const APP = {
     deferredInstall: null,
     SW: null,
-    cacheName: 'AccoLari-v0.1',
+    cacheName: 'AccoLari-v0.2',
     init() {
         if ('serviceWorker' in navigator) {
             navigator.serviceWorker.register('sw.js').then((registration) => {
